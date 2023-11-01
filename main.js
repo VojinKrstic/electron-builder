d096dffd154efd059e0240fe1b545b3f01107b8c
Stefan Vojkic <stefan@vrize.com>
2023-11-01 18:06:57 +0100

    run i bild sa detached processom

---
+++ main.js
@@ -0,0 +1,74 @@
+const { app, BrowserWindow } = require('electron');
+const { spawn } = require('node:child_process');
+const path = require('node:path');
+
+let nextServerProcess;
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        }
+    })
+
+    win.loadFile(path.join(__dirname, 'public/loading.html'))
+
+    // Moze i ne mora da bude ovde, ako se pre pokrene rucno
+    const nextBuildProcess = spawn('npm', ['run', 'build'], {
+        shell: true,
+    });
+
+    nextBuildProcess.on('exit', (code) => {
+        if (code !== 0) {
+            console.error('Next Build Failed!');
+        }
+
+       nextServerProcess = spawn('npm', ['start'], {
+            shell: true,
+            detached: true, // da bi mogao da se ugasi server inace zauzme port do reseta racunara
+        });
+
+        nextServerProcess.stdout.on('data', (data) => {
+            console.log(data.toString());
+        });
+
+        nextServerProcess.stderr.on('data', (data) => {
+            console.error(data.toString());
+        });
+
+        win.loadURL('http://localhost:3003');
+    });
+
+    win.webContents.openDevTools();
+
+    // win.on('closed', () => {
+    //     console.log('zatvara', nextServerProcess.pid, nextServerProcess.killed);
+    //     kill(nextServerProcess.pid);
+    //     console.log('kenny: ', nextServerProcess.killed);
+    // })
+}
+
+app.whenReady().then(() => {
+    createWindow();
+})
+
+app.on('before-quit', () => {
+    console.log(nextServerProcess.pid);
+    nextServerProcess.kill(); // ne radi????
+});
+
+app.on('window-all-closed', () => {
+    // if (nextServerProcess) {
+    //     console.log('kill!!!', nextServerProcess, nextServerProcess.pid);
+    //     process.kill(nextServerProcess.pid); ne radi
+    // }
+    if (process.platform !== 'darwin') app.quit()
+})
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
