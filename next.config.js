891e342c8461e205650afac9aa8c34f280e2e10b
Vojin Krstic <vojinkrstic1@gmail.com>
2023-11-23 15:36:11 +0100

    first try caching whole website

--- next.config.js
+++ next.config.js
@@ -3,4 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
+
 module.exports = nextConfig;
